81bb1fa78a049a7b551152b240df14f35e03e112
Rivers <zhaoxiang0805@hotmail.com>
2018-03-13 20:40:40 +0800

    pseudo bio sample

---
+++ src/main/java/vip/yoxiang/sample4PseudoIO/TimeServer.java
@@ -0,0 +1,44 @@
+package vip.yoxiang.sample4PseudoIO;
+
+import vip.yoxiang.sample4bio.TimeServerHandler;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Author: RiversLau
+ * Date: 2018/3/13 20:19
+ */
+public class TimeServer {
+
+    private static final Integer DEFAULT_PORT = 8888;
+
+    public static void main(String[] args) throws IOException {
+        int port = DEFAULT_PORT;
+        if (args != null && args.length > 0) {
+            try {
+                port = Integer.valueOf(args[0]);
+            } catch (NumberFormatException e) {
+                // do nothing
+            }
+        }
+
+        ServerSocket server = null;
+        try {
+            server = new ServerSocket(port);
+            System.out.println("The time server will start with the port:" + port);
+            Socket socket;
+            TimeServerHandlerExecutePool executePool = new TimeServerHandlerExecutePool(50, 10000);
+            while (true) {
+                socket = server.accept();
+                executePool.execute(new TimeServerHandler(socket));
+            }
+        } finally {
+            if (server != null) {
+                server.close();
+                System.out.println("The time server shut down.");
+            }
+        }
+    }
+}
---
+++ src/main/java/vip/yoxiang/sample4PseudoIO/TimeServerHandlerExecutePool.java
@@ -0,0 +1,24 @@
+package vip.yoxiang.sample4PseudoIO;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Author: RiversLau
+ * Date: 2018/3/13 20:16
+ */
+public class TimeServerHandlerExecutePool {
+
+    private ExecutorService executor;
+
+    public TimeServerHandlerExecutePool(int maxPoolSize, int queueSize) {
+        executor = new ThreadPoolExecutor(Runtime.getRuntime().availableProcessors(), maxPoolSize, 120L,
+                TimeUnit.SECONDS, new ArrayBlockingQueue<Runnable>(queueSize));
+    }
+
+    public void execute(Runnable task) {
+        executor.execute(task);
+    }
+}
