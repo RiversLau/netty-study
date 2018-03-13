408dd17f651edeb2df0469653aca5056df9b504e
Rivers <zhaoxiang0805@hotmail.com>
2018-03-13 18:06:09 +0800

    bio sample

---
+++ src/main/java/vip/yoxiang/sample4bio/TimeClient.java
@@ -0,0 +1,60 @@
+package vip.yoxiang.sample4bio;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+/**
+ * Author: RiversLau
+ * Date: 2018/3/13 16:44
+ */
+public class TimeClient {
+
+    private static final Integer DEFAULT_PORT = 8888;
+
+    public static void main(String[] args) {
+        int port = DEFAULT_PORT;
+        if (args != null && args.length > 0) {
+            try {
+                port = Integer.valueOf(args[0]);
+            } catch (NumberFormatException e) {
+                // do nothing
+            }
+        }
+
+        Socket socket = null;
+        BufferedReader in = null;
+        PrintWriter out = null;
+        try {
+            socket = new Socket("127.0.0.1", port);
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new PrintWriter(socket.getOutputStream(), true);
+            out.println("QUERY TIME ORDER");
+            System.out.println("Send order 2 server succeed!");
+            String resp = in.readLine();
+            System.out.println("Now is " + resp);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (out != null) {
+                out.close();
+            }
+            if (socket != null) {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/vip/yoxiang/sample4bio/TimeServer.java
@@ -0,0 +1,41 @@
+package vip.yoxiang.sample4bio;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Author: RiversLau
+ * Date: 2018/3/13 16:23
+ */
+public class TimeServer {
+
+    private static final Integer DEFAULT_PORT = 8888;
+
+    public static void main(String[] args) throws IOException {
+        int port = DEFAULT_PORT;
+        if (args != null && args.length > 0) {
+            try {
+                port = Integer.valueOf(args[0]);
+            } catch (NumberFormatException e) {
+                // do nothing
+            }
+        }
+
+        ServerSocket server = null;
+        try {
+            server = new ServerSocket(port);
+            System.out.println("The time server will start with the port:" + port);
+            Socket socket;
+            while (true) {
+                socket = server.accept();
+                new Thread(new TimeServerHandler(socket)).start();
+            }
+        } finally {
+            if (server != null) {
+                server.close();
+                System.out.println("The time server shut down.");
+            }
+        }
+    }
+}
