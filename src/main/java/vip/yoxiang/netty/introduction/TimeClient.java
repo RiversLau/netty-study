cb2e526e70f912721db614b91470a5d61789ff57
LiuRivers <zhaoxiang0805@hotmail.com>
2018-03-15 07:37:05 +0800

    netty introduction

---
+++ src/main/java/vip/yoxiang/netty/introduction/TimeClient.java
@@ -0,0 +1,51 @@
+package vip.yoxiang.netty.introduction;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+/**
+ * Author: Rivers
+ * Date: 2018/3/15 07:17
+ */
+public class TimeClient {
+
+    private static final Integer DEFAULT_PORT = 8888;
+
+    public static void main(String[] args) throws InterruptedException {
+        int port = DEFAULT_PORT;
+        if (args != null && args.length > 0) {
+            try {
+                port = Integer.valueOf(args[0]);
+            } catch (NumberFormatException e) {
+                // do nothing
+            }
+        }
+
+        new TimeClient().connect(port, "127.0.0.1");
+    }
+
+    public void connect(int port, String host) throws InterruptedException {
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            Bootstrap b = new Bootstrap();
+            b.group(group).channel(NioSocketChannel.class)
+                    .option(ChannelOption.TCP_NODELAY, true)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            socketChannel.pipeline().addLast(new TimeClientHandler());
+                        }
+                    });
+
+            ChannelFuture f = b.connect(host, port).sync();
+            f.channel().closeFuture().sync();
+        } finally {
+            group.shutdownGracefully();
+        }
+    }
+}
---
+++ src/main/java/vip/yoxiang/netty/introduction/TimeClientHandler.java
@@ -0,0 +1,11 @@
+package vip.yoxiang.netty.introduction;
+
+import io.netty.channel.ChannelHandlerAdapter;
+
+/**
+ * Author: Rivers
+ * Date: 2018/3/15 07:22
+ */
+public class TimeClientHandler extends ChannelHandlerAdapter {
+
+}
